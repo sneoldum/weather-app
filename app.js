b14bdcea98e4fe7ee85a5f943fa4b561c38b38cb
sneoldum <neoldums@gmail.com>
2023-10-25 01:33:02 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,52 +1,52 @@
-const apikey = "3265874a2c77ae4a04bb96236a642d2f";
+const apikey = "27372a1908b8187cc26cfd40aa6e4073";
 
 const main = document.getElementById("main");
 const form = document.getElementById("form");
 const search = document.getElementById("search");
 console.log(search);
 const url = (city) =>
-          `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`;
+  `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`;
 
 
 
 async function getWeatherByLocation(city) {
 
-          const resp = await fetch(url(city), { origin: "cors" });
-          const respData = await resp.json();
+  const resp = await fetch(url(city), { origin: "cors" });
+  const respData = await resp.json();
 
-          console.log(respData);
+  console.log(respData);
 
-          addWeatherToPage(respData);
+  addWeatherToPage(respData);
 }
 
 function addWeatherToPage(data) {
-          const temp = KtoC(data.main.temp);
+  const temp = KtoC(data.main.temp);
 
-          const weather = document.createElement("div");
-          weather.classList.add("weather");
+  const weather = document.createElement("div");
+  weather.classList.add("weather");
 
-          weather.innerHTML = `
+  weather.innerHTML = `
         <h2><img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /> ${temp}°C <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /></h2>
         <small>${data.weather[0].main}</small>
     `;
 
-          // cleanup
-          main.innerHTML = "";
+  // cleanup
+  main.innerHTML = "";
 
-          main.appendChild(weather);
+  main.appendChild(weather);
 }
 
 function KtoC(K) {
-          return Math.floor(K - 273.15);
+  return Math.floor(K - 273.15);
 }
 
 form.addEventListener("submit", (e) => {
-          e.preventDefault();
+  e.preventDefault();
 
-          const city = search.value;
+  const city = search.value;
 
-          if (city) {
-                    console.log(city);
-                    getWeatherByLocation(city);
-          }
+  if (city) {
+    console.log(city);
+    getWeatherByLocation(city);
+  }
 });
